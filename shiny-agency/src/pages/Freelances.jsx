8070dcf804ba6b5c758c0852af006373c77fcae7
Lukam90 <lucien.hamm@gmail.com>
2022-10-09 17:48:46 +0200

    MAJ Ex React > API

--- shiny-agency/src/pages/Freelances.jsx
+++ shiny-agency/src/pages/Freelances.jsx
@@ -1,9 +1,14 @@
+import { useState, useEffect } from "react"
+
 import styled from "styled-components"
 
 import colors from '../utils/style/colors'
 
 import Card from '../components/Card'
- 
+
+import { Loader } from '../utils/style/Atoms'
+
+/*
 const freelanceProfiles = [
     {
         name: 'Jane Doe',
@@ -18,6 +23,7 @@ const freelanceProfiles = [
         jobTitle: 'Développeuse Fullstack',
     },
 ]
+*/
 
 const CardsContainer = styled.div`
     display: grid;
@@ -41,7 +47,41 @@ const PageSubtitle = styled.h2`
     padding-bottom: 30px;
 `
 
+const LoaderWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 function Freelances() {
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(false)
+    const [freelancersList, setFreelancesList] = useState([])
+
+    useEffect(() => {
+        async function fetchFreelances() {
+            setDataLoading(true)
+
+            try {
+                const response = await fetch(`http://localhost:8000/freelances`)
+                const { freelancersList } = await response.json()
+
+                setFreelancesList(freelancersList)
+            } catch (err) {
+                console.log('===== error =====', err)
+
+                setError(true)
+            } finally {
+                setDataLoading(false)
+            }
+        }
+
+        fetchFreelances()
+    }, [])
+
+    if (error) {
+        return <span>Oups, il y a une erreur !</span>
+    }
+
     return (
         <div>
             <PageTitle>Trouvez votre prestataire</PageTitle>
@@ -50,15 +90,22 @@ function Freelances() {
             Chez Shiny nous réunissons les meilleurs profils pour vous.
             </PageSubtitle>
 
-            <CardsContainer>
-                {freelanceProfiles.map((profile, index) => (
-                    <Card
-                        key={index}
-                        label={profile.jobTitle}
-                        title={profile.name}
-                    />
-                ))}
-            </CardsContainer>
+            {isDataLoading ? (
+                <LoaderWrapper>
+                    <Loader />
+                </LoaderWrapper>
+            ) : (
+                <CardsContainer>
+                    {freelancersList.map((profile, index) => (
+                        <Card
+                            key={index}
+                            label={profile.jobTitle}
+                            title={profile.name}
+                            picture={profile.picture}
+                        />
+                    ))}
+                </CardsContainer>
+            )}
         </div>
     )
 }
--- shiny-agency/src/pages/Survey.jsx
+++ shiny-agency/src/pages/Survey.jsx
@@ -1,6 +1,39 @@
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+import styled from 'styled-components'
+
+import colors from '../utils/style/colors'
+
+import { Loader } from '../utils/style/Atoms'
+
+const SurveyContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+    text-decoration: underline;
+    text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+    margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+    padding-top: 30px;
+
+    & a {
+        color: black;
+    }
+    
+    & a:first-of-type {
+        margin-right: 20px;
+    }
+`
 
 function Survey() {
     const { questionNumber } = useParams()
@@ -9,20 +42,56 @@ function Survey() {
     const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
     const nextQuestionNumber = questionNumberInt + 1
 
+    const [surveyData, setSurveyData] = useState([])
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        async function fetchSurvey() {
+            setDataLoading(true)
+
+            try {
+                const response = await fetch("http://localhost:8000/survey")
+
+                const { surveyData } = await response.json()
+
+                setSurveyData(surveyData)
+            } catch (err) {
+                console.log(err)
+
+                setError(true)
+            } finally {
+                setDataLoading(false)
+            }
+        }
+
+        fetchSurvey()
+    }, [])
+
+    if (error) {
+        return <span>Oups, il y a une erreur !</span>
+    }
+
     return (
-        <div>
-            <h1>Questionnaire</h1>
+        <SurveyContainer>
+            <QuestionTitle>Question {questionNumber}</QuestionTitle>
 
-            <h2>Question {questionNumber}</h2>
-
-            <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-
-            {questionNumberInt === 10 ? (
-                <Link to="/results">Résultats</Link>
+            {isDataLoading ? (
+                <Loader />
             ) : (
-                <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
             )}
-        </div>
+
+            <LinkWrapper>
+                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+
+                {surveyData[questionNumberInt + 1] ? (
+                    <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                ) : (
+                    <Link to="/results">Résultats</Link>
+                )}
+            </LinkWrapper>
+        </SurveyContainer>
     )
 }
 
