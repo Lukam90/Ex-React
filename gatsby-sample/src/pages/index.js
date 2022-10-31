090e11cbc3b8fafdebf64744dc91dd5f1bb3753f
Lukam90 <lucien.hamm@gmail.com>
2022-10-31 16:36:02 +0100

    MAJ Ex Gatsby > Navbar

--- gatsby-sample/src/pages/index.js
+++ gatsby-sample/src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
 
+import Navbar from "../components/Navbar"
+
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<section>
+			<Navbar />
+
+			<div>
+				<h2>Design</h2>
+
+				<h3>Develop & Deploy</h3>
+
+				<p>UX design & web developer</p>
+			</div>
+		</section>
+	)
 }
--- shiny-agency/src/utils/style/colors.js
+++
@@ -1,7 +0,0 @@
-const colors = {
-    primary: "#5843e4",
-    secondary: "#8186a0",
-    backgroundLight: "#f9f9fc"
-}
-
-export default colors
