24a8dbaf16f6fb6bbdbfa618a9f71348b181477e
Lukam90 <lucien.hamm@gmail.com>
2022-11-06 21:10:19 +0100

    MAJ Ex MERN > Controllers 2/2

--- mern-stack/backend/routes/workouts.js
+++ mern-stack/backend/routes/workouts.js
@@ -4,6 +4,8 @@ const {
     getWorkouts,
     getWorkout,
     createWorkout,
+    updateWorkout,
+    deleteWorkout
 } = require("../controllers/workoutController")
 
 const router = express.Router()
@@ -18,13 +20,9 @@ router.get("/:id", getWorkout)
 router.post("/", createWorkout)
 
 // UPDATE a workout
-router.patch("/:id", (req, res) => {
-    res.json({ message: "UPDATE a workout" })
-})
+router.patch("/:id", updateWorkout)
 
 // DELETE a workout
-router.delete("/:id", (req, res) => {
-    res.json({ message: "DELETE a workout" })
-})
+router.delete("/:id", deleteWorkout)
 
 module.exports = router
