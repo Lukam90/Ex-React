d3df922632d73c0372c7510d87e24735c987b5e3
Lukam90 <lucien.hamm@gmail.com>
2023-11-11 20:54:54 +0100

    MAJ Ex MUI > Navbar

--- mui-blog-course/src/components/Navbar.jsx
+++ mui-blog-course/src/components/Navbar.jsx
@@ -1,10 +1,55 @@
-import Box from '@mui/material/Box';
+import { styled, AppBar, Toolbar, Typography, Box, InputBase, Badge, Avatar } from '@mui/material';
+import { Pets, Mail, Notifications } from "@mui/icons-material";
+
+const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between"
+});
+
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%"
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+    display: "none",
+    alignItems: "center",
+    gap: "20px",
+    [theme.breakpoints.up("sm")]: {
+        display: "flex"
+    }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px"
+}));
 
 const Navbar = () => {
     return (
-        <Box>
-        
-        </Box>
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>LAMA DEV</Typography>
+                <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+                <Search>
+                    <InputBase placeholder='Search...' />
+                </Search>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+
+                    <Badge badgeContent={2} color="error">
+                        <Notifications />
+                    </Badge>
+
+                    <Avatar src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress" />
+                </Icons>
+            </StyledToolbar>
+        </AppBar>
     )
 }
 
