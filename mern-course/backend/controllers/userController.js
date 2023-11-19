09ff2553a3187cfb216b059154623bc2b33f5bbc
Lukam90 <lucien.hamm@gmail.com>
2023-11-19 18:41:43 +0100

    MAJ Ex MERN > User Auth

--- mern-course/backend/controllers/userController.js
+++ mern-course/backend/controllers/userController.js
@@ -8,6 +8,23 @@ const User = require("../models/userModel")
 // @route  POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({ email })
+
+    if (user && await bcrypt.compare(password, user.password)) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+
+        throw new Error("Invalid credentials")
+    }
+
     res.json({ message: "Login User" })
 })
 
@@ -30,6 +47,29 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
 
         throw new Error("User already exists")
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+
+        throw new Error("Invalid user data")
     }
 
     res.json({ message: "Register User" })
