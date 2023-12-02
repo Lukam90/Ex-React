a339e4558557338b4fc10030e1cef8c06902b982
Lukam90 <lucien.hamm@gmail.com>
2023-12-02 20:57:24 +0100

    MAJ Ex Todos > Button Toggle

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 
-import Button from './components/Button';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+	const [showAddTask, setShowAddTask] = useState(false)
+
 	const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -26,6 +28,14 @@ function App() {
 		}
 	])
 
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1
+
+		const newTask = { id, ...task }
+
+		setTasks([...tasks, newTask])
+	}
+
 	const deleteTask = (id) => {
 		setTasks(tasks.filter(task => task.id !== id))
 	}
@@ -40,7 +50,13 @@ function App() {
 
   	return (
 		<div className="container">
-			<Header />
+			<Header 
+				onAdd={() => setShowAddTask(! showAddTask)} 
+				showAdd={showAddTask}
+			/>
+
+			{showAddTask && <AddTask onAdd={addTask} />}
+
 			{tasks.length > 0 ? (
 				<Tasks 
 					tasks={tasks} 
